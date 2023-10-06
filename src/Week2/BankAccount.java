a0548790877b989c1a28b31cb4769ed552eddcce
ajay24000 <ajaykm1600@gmail.com>
2023-10-06 14:56:44 +0530

    Merge remote-tracking branch 'origin/master'

--- src/Week2/BankAccount.java
+++ src/Week2/BankAccount.java
@@ -1,16 +1,27 @@
 package Week2;
 
+
 public class BankAccount {
-    private int balance;
+    private double balance;
     BankAccount(){
         this.balance=0;
     }
-    BankAccount(int accountBalance){
+    BankAccount(double accountBalance){
         this.balance=accountBalance;
     }
+    double getAccountBalance() {
+        return balance;
+    }
+    public void setAccountBalance(double accountBalance) {
+        this.balance = accountBalance;
+    }
+
 
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount(1000);
-        BankAccount account2 = new BankAccount();
+        BankAccount account1 = new BankAccount();
+        BankAccount account2 = new BankAccount(4500);
+        account2.setAccountBalance(1000);
+        System.out.println(account1.getAccountBalance());
+        System.out.println(account2.getAccountBalance());
     }
 }
