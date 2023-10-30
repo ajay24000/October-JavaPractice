fddbcc5c4c38a389ea076f2be0a51f610422ebf1
Ajay Kumar <143401137+ajay24000@users.noreply.github.com>
2023-10-30 09:12:06 +0530

    Week-3 / Exercise-4

---
+++ Exercise-4/src/main/java/com/configuration/MicroserviceA.java
@@ -0,0 +1,12 @@
+package com.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceA {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceA.class,args);
+    }
+
+}
