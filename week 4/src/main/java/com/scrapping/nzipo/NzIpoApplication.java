de8be2f18072541aea3edb7c529741f1872ee676
ajay24000 <ajaykm1600@gmail.com>
2023-11-24 15:38:39 +0530

    repushing question 3/week 4 in week 4 folder

---
+++ week 4/src/main/java/com/scrapping/nzipo/NzIpoApplication.java
@@ -0,0 +1,13 @@
+package com.scrapping.nzipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzIpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NzIpoApplication.class, args);
+	}
+
+}
---
+++ week 4/src/test/java/com/scrapping/nzipo/NzIpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapping.nzipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzIpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
